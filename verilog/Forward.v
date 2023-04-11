81af511f7042777669dc7f64ef327e643e9c9f02
crapman5389 <fjdurso3@gmail.com>
2023-04-11 12:43:13 -0400

    Update Forward.v
    Described the inputs and outputs for the forwarding unit

--- verilog/Forward.v
+++ verilog/Forward.v
@@ -1,10 +1,15 @@
 module Forward(
 
 	//MODULE INPUTS
-
-
+		input [4:0] ID/EX.RegisterRt,
+		input [4:0] ID/EX.RegisterRs,
+		input [4:0] MEM/WB.RegisterRd,
+		input EX/MEM.RegWrite,
+		input MEM/WB.RegWrite,
 
 	//MODULE OUTPUTS
+		output [1:0] ForwardA,
+		output [1:0] ForwardB
 
 
 
