920bdd3b043290ec82284a00baeba3a7923037c9
fdurso <43616069+fdurso@users.noreply.github.com>
2023-04-17 21:37:05 -0400

    forwarding logic

--- verilog/Forward.v
+++ verilog/Forward.v
@@ -1,16 +1,22 @@
 module Forward(
 
 	//MODULE INPUTS
-		input [4:0] ID/EX.RegisterRt,
-		input [4:0] ID/EX.RegisterRs,
-		input [4:0] MEM/WB.RegisterRd,
-		input EX/MEM.RegWrite,
-		input MEM/WB.RegWrite,
+		input [4:0] IDEX.RegisterRt,
+		input [4:0] IDEX.RegisterRs,
+		input [4:0] MEMWB.RegisterRd,
+		input [4:0] EXMEM.RegisterRd,
+		input EXMEM.RegWrite,
+		input MEMWB.RegWrite,
 
 	//MODULE OUTPUTS
 		output [1:0] ForwardA,
 		output [1:0] ForwardB
+		
+assign ForwardA[0] = (EXMEM.RegWrite & !(EXMEM.RegisterRd == 0) & (EXMEM.RegisterRd = IDEX.RegisterRs))
+assign ForwardA[1] = (MEMWB.RegWrite & !(MEMWB.RegisterRd == 0) & (MEMWB.RegisterRd = IDEX.RegisterRs))
 
+
+assign ForwardB = (EXMEM.RegWrite & !(EXMEM.RegisterRd == 0) & (EXMEM.RegisterRd = IDEX.RegisterRt)) || (MEMWB.RegWrite & !(MEMWB.RegisterRd == 0) & (MEMWB.RegisterRd = IDEX.RegisterRt)) 
 
 
 );
