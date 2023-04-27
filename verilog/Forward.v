b048c2353264f8a04bc1d9a38af3a2039899bbbf
purpleman123 <80710001+purpleman123@users.noreply.github.com>
2023-04-27 15:27:03 -0400

    Forward updates

--- verilog/Forward.v
+++ verilog/Forward.v
@@ -4,14 +4,11 @@ module Forward(
 		input [31:0] Instruction_IN,
 		input [4:0]  EXMEM_RegD,
 		input [4:0]  MEMWB_RegD,
-		input [31:0] EXMEM_RegD_Value,
-		input [31:0] MEMWB_RegD_Value,
 		input        CLOCK,
 
 	//MODULE OUTPUTS
 		output [31:0] Forward_A,
 		output [31:0] Forward_B
-
 );
 
 wire [4:0]     	RegisterRS;    
@@ -29,19 +26,19 @@ always @(posedge CLOCK) begin
 
 	// EXMEM RD == IDEX RS
 	if(EXMEM_Rd == RegisterRS)begin
-		Forward_A = EXMEM_RegD_Value;
+		Forward_A = 2'b01;
 	end
 	// EXEMEM RD == IDEX RT
 	if(EXMEM_Rd == RegisterRT)begin
-		Forward_B = EXMEM_RegD_Value;
+		Forward_B = 2'b10;
 	end
 	// MEMWB RD == IDEX RS
 	if(MEMWB_Rd == RegisterRS)begin
-		Forward_A = MEMWB_RegD_Value;
+		Forward_A = 2'b01;
 	end
 	// MEMWB RD == IDEX RT
 	if(MEMWB_Rd == RegisterRT)begin
-		Forward_B = MEMWB_RegD_Value;
+		Forward_B = 2'b10;
 	end
 end
 
